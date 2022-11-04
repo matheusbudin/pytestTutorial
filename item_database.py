df24435a7eaef84430f9b45a7de01965cd80434f
mbudin <mbudin@brasilseg.com.br>
2022-11-04 15:23:15 -0300

    final commentary

--- item_database.py
+++ item_database.py
@@ -1,3 +1,6 @@
+#this file is an example when a team is working in different parts
+#we need this database for our price_map, but we cant wait  for it to be implemented
+#thats why we have to use the mocking function
 class ItemDatabase:
     def __init__(self) -> None:
         pass
